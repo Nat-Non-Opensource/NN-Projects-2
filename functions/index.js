62387800c7544a11524117afbd36b823c30114b3
Nat <nat.wrw@gmail.com>
2020-06-16 12:31:15 +0700

    update

--- functions/index.js
+++ functions/index.js
@@ -5,22 +5,35 @@ const spawn = require("child-process-promise").spawn;
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
-const express = require("express");
 const cors = require("cors")({
   origin: true
 });
+//const express = require("express");
+//const cors = require("cors")({
+//  origin: true
+//});
+//
+//const app = express();
+//const router = express.Router();
+//app.use(cors);
+//app.use("/", router);
+//app.get("/status", (req, res) => {
+//  console.log(`headers:`, req.headers);
+//  res.status(200).send("OK");
+//});
+//
+//module.exports = app;
 
-const app = express();
-const router = express.Router();
-app.use(cors);
-app.use("/", router);
-app.get("/status", (req, res) => {
-  console.log(`headers:`, req.headers);
-  res.status(200).send("OK");
+exports.bigben = functions.https.onRequest((req, res) => {
+  cors((req, res, () => {
+    const hours = (new Date().getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${"BONG ".repeat(hours)}
+    </body>
+  </html>`);
+  }));
 });
-
-module.exports = {
-  app: app
-};
-
-
